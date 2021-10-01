f681f74121e9d98a03576a25be4f7addfcdf31a9
andrezej <a.husiev@fudosecurity.com>
2021-10-01 07:55:00 +0200

    Add quotes form component

---
+++ src/components/QuotesForm.jsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+const QuotesForm = () => (
+  <QuotesFormStyled>
+    <FormGroupStyled>
+      <label>Quote</label>
+      <InputStyled type='text' />
+    </FormGroupStyled>
+
+    <FormGroupStyled>
+      <label>Author</label>
+      <InputStyled type='text' />
+    </FormGroupStyled>
+
+    <FormGroupStyled>
+      <SubmitButtonStyled>Add</SubmitButtonStyled>
+    </FormGroupStyled>
+  </QuotesFormStyled>
+);
+
+const QuotesFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  margin: auto;
+`;
+
+const InputStyled = styled.input`
+  border: none;
+  padding: 6px 12px;
+  border-radius: 8px;
+  background: #f0f0f0;
+`;
+
+const FormGroupStyled = styled.div`
+  display: grid;
+  grid-template-columns: 50px 1fr;
+  align-items: center;
+  grid-gap: 10px;
+  padding: 15px 10px 0;
+`;
+
+const SubmitButtonStyled = styled.button`
+  grid-column: 3/-1;
+  padding: 6px 12px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  background: #f0f0f0;
+  transition: all 0.2s;
+
+  &:hover {
+    background: #ccc;
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+export default QuotesForm;
